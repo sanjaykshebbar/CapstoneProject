9078f6a233b9448169d1c623023c20be1cffe5a5
Sanjay KS <98266129+sanjaykshebbar@users.noreply.github.com>
2024-11-06 07:00:17 +0530

    Update terraform.tf
    Updated the terraform code

--- terraform.tf
+++ terraform.tf
@@ -10,24 +10,39 @@ resource "aws_instance" "docker_host" {
   ami           = "ami-0866a3c8686eaeeba" # Example AMI (Ubuntu)
   instance_type = "t2.micro"
   key_name      = "DevOps-Capstone-Project"
+  security_groups = ["web_sg"]
 
   tags = {
-    Name = "DockerHost"
+    Name = "web-server"
   }
-
-  # Security group to allow SSH and HTTP
-  security_groups = ["docker-sg"]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install docker.io -y
-              sudo systemctl start docker
-              sudo systemctl enable docker
-              EOF
 }
 
-# Output the EC2 instance's public IP
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 output "ec2_public_ip" {
-  value = aws_instance.docker_host.public_ip
+  value = aws_instance.web_server.public_ip
 }
